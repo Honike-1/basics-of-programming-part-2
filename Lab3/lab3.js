77d52c86f71f1552841cd4a33de56f19a04d9dae
Honike-1 <romakizilenkov@gmail.com>
2026-03-18 23:52:47 +0200

    made main part of lab3

--- Lab3/lab3.js
+++ Lab3/lab3.js
@@ -17,7 +17,7 @@ function memoize(fn, options = {}) {
 
     for (const [key, entry] of cache) {
       if (entry.freq < minFrequency) {
-        minFrequency = entry.freq; // потім пропишу freq
+        minFrequency = entry.freq;
         LFUKey = key;
       }
     }
@@ -27,9 +27,57 @@ function memoize(fn, options = {}) {
   function timeout() {
     for (const [key, entry] of cache) {
       if (Date.now() - entry.addedAt > time) {
-        // потім напишу addedAt
         cache.delete(key);
       }
     }
   }
+
+  function eviction() {
+    if (cache.size <= maxSize) {
+      return;
+    }
+
+    if (policy === "LRU") {
+      LRU();
+    } else if (policy === "LFU") {
+      LFU();
+    } else if (policy === "timeout") {
+      timeout();
+    }
+  }
+
+  return function (...args) {
+    const key = makeKey(args);
+
+    if (policy === "timeout" && cache.has(key)) {
+      const entry = cache.get(key);
+      if (Date.now() - entry.addedAt > time) {
+        cache.delete(key);
+      }
+    }
+
+    if (cache.has(key)) {
+      const entry = cache.get(key);
+      entry.freq = (entry.freq || 0) + 1;
+      if (policy === "LRU") {
+        cache.delete(key);
+        cache.set(key, entry);
+      }
+
+      return entry.value;
+    }
+
+    const result = fn.apply(this, args);
+
+    cache.set(key, {
+      value: result,
+      freq: 1,
+      addedAt: Date.now(),
+    });
+
+    eviction();
+    return result;
+  };
 }
+
+export { memoize };
