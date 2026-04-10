2532c46d7e85667a648f9c21da83d99c5ba26462
Honike-1 <romakizilenkov@gmail.com>
2026-04-10 18:02:00 +0300

    finished lab 5

--- Lab5/Lab5.py
+++ Lab5/Lab5.py
@@ -1,4 +1,5 @@
 import asyncio
+import re
 
 def map1(arr, fn, callback):
     async def _run():
@@ -12,7 +13,7 @@ def map1(arr, fn, callback):
     
     def _execute():
         try:
-            loop = asyncio.get_running_loop()
+            loop = asyncio.get_event_loop()
             result = loop.run_until_complete(_run())
             callback(None, result)
         except Exception as error:
@@ -30,3 +31,39 @@ async def map2(arr, fn, stop_event=None):
         result.append(res)
     
     return result
+
+
+# demo 1
+
+arr1 = ["idk", "words", "something", "brainrot"]
+async def fn1(item):
+    await asyncio.sleep(1)
+    return len(item)
+
+def callback(error, result):
+    if error:
+        print("Error:", error)
+    else:
+        print("Words length:", result)
+        
+map1(arr1, fn1, callback)
+
+# demo 2
+
+arr2 = ["email@gmail.com", "smth", "another_email@ukr.net", "smth2"]
+async def fn2(item):
+    await asyncio.sleep(1)
+    is_valid = bool(re.match(r"[^@]+@[^@]+\.[^@]+", item))
+    return {"email": item, "valid": is_valid}
+
+async def demo2():
+    
+    res = await map2(arr2, fn2)
+
+    for item in res:
+        if item["valid"] == True:
+            print(f"{item["email"]}: is valid")
+        else:
+            print(f"{item["email"]}: is not valid")
+            
+asyncio.run(demo2())
